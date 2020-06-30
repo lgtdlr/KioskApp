1f55d63a1cee6863d75b9046fb5b8276ced952f3
ash-77696e <haridas.ashwin@gmail.com>
2020-06-30 13:11:28 -0400

    Added face detection (rectangles) without API call to live camera

--- app/src/main/java/com/example/kioskapp/DetectCameraActivity.java
+++ app/src/main/java/com/example/kioskapp/DetectCameraActivity.java
@@ -2,6 +2,7 @@ package com.example.kioskapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
@@ -10,19 +11,33 @@ import android.view.WindowManager;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
-
-import static android.content.ContentValues.TAG;
 
 public class DetectCameraActivity extends CameraActivity implements CvCameraViewListener2 {
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    JavaCameraView javaCameraView;
+    File cascFile;
+    CascadeClassifier faceDetector;
+
+    private Mat mRgba, mGray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +47,72 @@ public class DetectCameraActivity extends CameraActivity implements CvCameraView
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.java_camera_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        if(!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, baseCallback);
+        } else {
+            try {
+                baseCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        javaCameraView.setCvCameraViewListener(this);
     }
+
 
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        //detect faces
+
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(mRgba, faceDetections);
+
+        for(Rect rect: faceDetections.toArray()) {
+            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(255, 0, 0));
+        }
+
+        return mRgba;
     }
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    private BaseLoaderCallback baseCallback = new BaseLoaderCallback(this) {
 
         @Override
-        public void onManagerConnected(int status) {
+        public void onManagerConnected(int status) throws IOException {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
-                    Log.i("TAG", "OpenCV loaded successfully");
-                    mOpenCvCameraView.enableView();
+                    InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+                    File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                    cascFile = new File(cascadeDir, "haarcascade_frontalface_alt2.xml");
+
+                    FileOutputStream fos = new FileOutputStream(cascFile);
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+
+                    while((bytesRead = is.read(buffer)) != -1) {
+                        fos.write(buffer, 0, bytesRead);
+                    }
+                    is.close();
+                    fos.close();
+
+                    faceDetector = new CascadeClassifier(cascFile.getAbsolutePath());
+
+                    if(faceDetector.empty()) {
+                        faceDetector = null;
+                    } else
+                        cascadeDir.delete();
+
+                    javaCameraView.enableView();
                 }
                 break;
+
                 default: {
                     super.onManagerConnected(status);
                 }
@@ -60,15 +124,6 @@ public class DetectCameraActivity extends CameraActivity implements CvCameraView
     @Override
     public void onResume () {
         super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-//        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,
-//                this, mLoaderCallback);
     }
 
     @Override
@@ -90,9 +145,13 @@ public class DetectCameraActivity extends CameraActivity implements CvCameraView
             mOpenCvCameraView.disableView();
     }
 
-    public void onCameraViewStarted ( int width, int height){
+    public void onCameraViewStarted (int width, int height) {
+        mRgba = new Mat();
+        mGray = new Mat();
     }
 
     public void onCameraViewStopped () {
+        mRgba.release();
+        mGray.release();
     }
 }
